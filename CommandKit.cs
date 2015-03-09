b2c0712525acfbef26c391c43af5990ec785bb8f
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-03-09 01:14:37 +0100

    Fixin :D

--- CommandKit.cs
+++ CommandKit.cs
@@ -19,20 +19,13 @@ namespace unturned.ROCKS.Kits
 
         protected override void execute(SteamPlayerID caller, string command)
         {
-            if (!RocketCommand.IsPlayer(caller)) return;
-
-            bool hasPermissions = RocketPermissionManager.GetPermissions(caller.CSteamID).Where(p => p.ToLower() == ("kit." + command.Trim().ToLower()) || p.ToLower() == "kit.*").FirstOrDefault() != null;
-
-            if (!hasPermissions)
+            if (String.IsNullOrEmpty(command.Trim()))
             {
-                RocketChatManager.Say(caller.CSteamID, "You don't have permissions to use that command");
+                RocketChatManager.Say(caller.CSteamID, "Invalid parameter, specify a kit with /kit <name>");
                 return;
             }
 
-            if (String.IsNullOrEmpty(command.Trim())) {
-                RocketChatManager.Say(caller.CSteamID,"Invalid parameter");
-                return;
-            }
+            if (!RocketCommand.IsPlayer(caller)) return;
 
             Kit kit = Kits.Configuration.Kits.Where(k => k.Name.ToLower() == command.Trim().ToLower()).FirstOrDefault();
             if (kit == null)
@@ -40,38 +33,46 @@ namespace unturned.ROCKS.Kits
                 RocketChatManager.Say(caller.CSteamID, "Kit not found");
                 return;
             }
-            else { 
-                Player player = PlayerTool.getPlayer(caller.CSteamID);
-                KitsPlayerComponent kp = player.transform.GetComponent<KitsPlayerComponent>();
 
-                double gt = (DateTime.Now - kp.GlobalKitCooldown).TotalSeconds;
-                if (gt < Kits.Configuration.GlobalCooldown)
-                {
-                    RocketChatManager.Say(caller.CSteamID, "You have to wait " + (Kits.Configuration.GlobalCooldown - gt) + " seconds to use this command again");
-                    return;
-                }
+            Player player = PlayerTool.getPlayer(caller.CSteamID);
+            bool hasPermissions = RocketPermissionManager.CheckPermissions(player.SteamChannel.SteamPlayer,"kit.*") || RocketPermissionManager.GetPermissions(caller.CSteamID).Where(p => p.ToLower() == ("kit." + command.Trim().ToLower())).FirstOrDefault() != null;
 
-                DateTime kitCooldown = kp.SpecificKitCooldown.ContainsKey(kit.Name.ToLower()) ? kp.SpecificKitCooldown[kit.Name] : DateTime.MinValue;
-
-                double kt = (DateTime.Now - kitCooldown).TotalSeconds;
-                if (gt < kit.Cooldown)
-                {
-                    RocketChatManager.Say(caller.CSteamID, "You have to wait " + (kit.Cooldown - gt) + " seconds to get this kit again");
-                    return;
-                }
-
-                foreach (KitItem item in kit.Items)
-                {
-                    if (!ItemTool.tryForceGiveItem(player, item.ItemId, item.Amount))
-                    {
-                        Logger.Log("Failed giving a item to " + caller.CharacterName + " (" + item.ItemId + "," + item.Amount + ")");
-                    }
-                }
-                RocketChatManager.Say(caller.CSteamID, "You just received the kit " + kit.Name);
-                kp.SpecificKitCooldown[kit.Name] = DateTime.Now;
-                kp.GlobalKitCooldown = DateTime.Now;
-
+            if (!hasPermissions)
+            {
+                RocketChatManager.Say(caller.CSteamID, "You don't have permissions to use this kit");
+                return;
             }
+
+            KitsPlayerComponent kp = player.transform.GetComponent<KitsPlayerComponent>();
+
+            double gt = (DateTime.Now - kp.GlobalKitCooldown).TotalSeconds;
+            if (gt < Kits.Configuration.GlobalCooldown)
+            {
+                RocketChatManager.Say(caller.CSteamID, "You have to wait " + (int)(Kits.Configuration.GlobalCooldown - gt) + " seconds to use this command again");
+                return;
+            }
+
+            DateTime kitCooldown = kp.SpecificKitCooldown.ContainsKey(kit.Name.ToLower()) ? kp.SpecificKitCooldown[kit.Name] : DateTime.MinValue;
+
+            double kt = (DateTime.Now - kitCooldown).TotalSeconds;
+            if (gt < kit.Cooldown)
+            {
+                RocketChatManager.Say(caller.CSteamID, "You have to wait " + (int)(kit.Cooldown - gt) + " seconds to get this kit again");
+                return;
+            }
+
+            foreach (KitItem item in kit.Items)
+            {
+                if (!ItemTool.tryForceGiveItem(player, item.ItemId, item.Amount))
+                {
+                    Logger.Log("Failed giving a item to " + caller.CharacterName + " (" + item.ItemId + "," + item.Amount + ")");
+                }
+            }
+            RocketChatManager.Say(caller.CSteamID, "You just received the kit " + kit.Name);
+            kp.SpecificKitCooldown[kit.Name] = DateTime.Now;
+            kp.GlobalKitCooldown = DateTime.Now;
+
+            
 
 
         }
