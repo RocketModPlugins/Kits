9efb3fdac9114f8e4a4c0c7e35bcf585939042f2
fr34kyn01535 <ihi4kah>
2015-03-28 23:32:58 +0100

    Update to Rocket 3.1

--- CommandKit.cs
+++ CommandKit.cs
@@ -8,59 +8,66 @@ using System.Linq;
 
 namespace unturned.ROCKS.Kits
 {
-    class CommandKit : Command
+    public class CommandKit : IRocketCommand
     {
-        public CommandKit()
+        public string Help
         {
-            base.commandName = "kit";
-            base.commandHelp = "Gives you a kit";
-            base.commandInfo = base.commandName + " - " + base.commandHelp;
+            get { return "Gives you a kit"; }
         }
 
-        protected override void execute(SteamPlayerID caller, string command)
+        public string Name
         {
+            get { return "kit";}
+        }
+
+        public bool RunFromConsole
+        {
+            get { return false; }
+        }
+
+        public void Execute(Steamworks.CSteamID caller, string command)
+        {
+            Player callingPlayer = PlayerTool.getPlayer(caller);
             if (String.IsNullOrEmpty(command.Trim()))
             {
-                RocketChatManager.Say(caller.CSteamID, Kits.Instance.Translate("command_kit_invalid_parameter"));
+                RocketChatManager.Say(caller, Kits.Instance.Translate("command_kit_invalid_parameter"));
                 return;
             }
-
-            if (!RocketCommand.IsPlayer(caller)) return;
 
             Kit kit = Kits.Instance.Configuration.Kits.Where(k => k.Name.ToLower() == command.Trim().ToLower()).FirstOrDefault();
             if (kit == null)
             {
-                RocketChatManager.Say(caller.CSteamID, Kits.Instance.Translate("command_kit_not_found"));
+                RocketChatManager.Say(caller, Kits.Instance.Translate("command_kit_not_found"));
                 return;
             }
 
-            Player player = PlayerTool.getPlayer(caller.CSteamID);
-            bool hasPermissions = RocketPermissionManager.CheckPermissions(player.SteamChannel.SteamPlayer,"kit.*") || RocketPermissionManager.GetPermissions(caller.CSteamID).Where(p => p.ToLower() == ("kit." + command.Trim().ToLower())).FirstOrDefault() != null;
+            Player player = PlayerTool.getPlayer(caller);
+            bool hasPermissions = RocketPermissionManager.CheckPermissions(player.SteamChannel.SteamPlayer,"kit.*") || RocketPermissionManager.GetPermissions(caller).Where(p => p.ToLower() == ("kit." + command.Trim().ToLower())).FirstOrDefault() != null;
 
             if (!hasPermissions)
             {
-                RocketChatManager.Say(caller.CSteamID, Kits.Instance.Translate("command_kit_no_permissions"));
+                RocketChatManager.Say(caller, Kits.Instance.Translate("command_kit_no_permissions"));
                 return;
             }
 
-            KeyValuePair<string, DateTime> globalCooldown = Kits.GlobalCooldown.Where(k => k.Key == caller.CSteamID.ToString()).FirstOrDefault();
+            KeyValuePair<string, DateTime> globalCooldown = Kits.GlobalCooldown.Where(k => k.Key == caller.ToString()).FirstOrDefault();
             if (!globalCooldown.Equals(default(KeyValuePair<string, DateTime>)))
             {
                 double globalCooldownSeconds = (DateTime.Now - globalCooldown.Value).TotalSeconds;
                 if (globalCooldownSeconds < Kits.Instance.Configuration.GlobalCooldown)
                 {
-                    RocketChatManager.Say(caller.CSteamID, Kits.Instance.Translate("command_kit_cooldown_command", (int)(Kits.Instance.Configuration.GlobalCooldown - globalCooldownSeconds)));
+                    RocketChatManager.Say(caller, Kits.Instance.Translate("command_kit_cooldown_command", (int)(Kits.Instance.Configuration.GlobalCooldown - globalCooldownSeconds)));
                     return;
                 }
             }
 
-            KeyValuePair<string, IndividualKitCooldown> individualCooldown = Kits.InvididualCooldown.Where(k => k.Key == caller.CSteamID.ToString() && k.Value.Kit == kit.Name).FirstOrDefault();
+            KeyValuePair<string, IndividualKitCooldown> individualCooldown = Kits.InvididualCooldown.Where(k => k.Key == caller.ToString() && k.Value.Kit == kit.Name).FirstOrDefault();
             if (!individualCooldown.Equals(default(KeyValuePair<string, IndividualKitCooldown>)))
             {
                 double individualCooldownSeconds = (DateTime.Now - individualCooldown.Value.Cooldown).TotalSeconds;
                 if (individualCooldownSeconds < kit.Cooldown)
                 {
-                    RocketChatManager.Say(caller.CSteamID, Kits.Instance.Translate("command_kit_cooldown_kit", (int)(kit.Cooldown - individualCooldownSeconds)));
+                    RocketChatManager.Say(caller, Kits.Instance.Translate("command_kit_cooldown_kit", (int)(kit.Cooldown - individualCooldownSeconds)));
                     return;
                 }
             }
@@ -69,13 +76,13 @@ namespace unturned.ROCKS.Kits
             {
                 if (!ItemTool.tryForceGiveItem(player, item.ItemId, item.Amount))
                 {
-                    Logger.Log(Kits.Instance.Translate("command_kit_failed_giving_item", caller.CharacterName, item.ItemId, item.Amount));
+                    Logger.Log(Kits.Instance.Translate("command_kit_failed_giving_item", callingPlayer.SteamChannel.SteamPlayer.SteamPlayerID.CharacterName, item.ItemId, item.Amount));
                 }
             }
-            RocketChatManager.Say(caller.CSteamID, Kits.Instance.Translate("command_kit_success", kit.Name));
+            RocketChatManager.Say(caller, Kits.Instance.Translate("command_kit_success", kit.Name));
 
-            Kits.GlobalCooldown.Add(caller.CSteamID.ToString(), DateTime.Now);
-            Kits.InvididualCooldown.Add(caller.CSteamID.ToString(), new IndividualKitCooldown() { Cooldown = DateTime.Now, Kit = kit.Name });
+            Kits.GlobalCooldown.Add(caller.ToString(), DateTime.Now);
+            Kits.InvididualCooldown.Add(caller.ToString(), new IndividualKitCooldown() { Cooldown = DateTime.Now, Kit = kit.Name });
         }
     }
 }
