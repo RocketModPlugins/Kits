10d2422a3dd5fc3f091987c9b1510a1d92c63fd6
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-07-31 03:24:04 +0200

    Default config

--- KitsConfiguration.cs
+++ KitsConfiguration.cs
@@ -1,18 +1,25 @@
 ﻿using Rocket.API;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using System;
 
 namespace unturned.ROCKS.Kits
 {
     public class KitsConfiguration : IRocketPluginConfiguration
     {
         [XmlArrayItem(ElementName = "Kit")]
-        public List<Kit> Kits = new List<Kit>() {
-            new Kit() { Cooldown = 10, Name = "Survival", Items = new List<KitItem>() { new KitItem(245, 1), new KitItem(81, 2), new KitItem(16, 1) }},
-            new Kit() { Cooldown = 10, Name = "Brute Force", Items = new List<KitItem>() { new KitItem(112, 1), new KitItem(113, 3), new KitItem(254, 3) }},
-            new Kit() { Cooldown = 10, Name = "Watcher", Items = new List<KitItem>() { new KitItem(109, 1), new KitItem(111, 3), new KitItem(236, 1) }}
-        };
-        public int GlobalCooldown = 10;
+        public List<Kit> Kits;
+        public int GlobalCooldown;
+
+        public void LoadDefaults()
+        {
+            GlobalCooldown = 10;
+            Kits = new List<Kit>() {
+                new Kit() { Cooldown = 10, Name = "Survival", Items = new List<KitItem>() { new KitItem(245, 1), new KitItem(81, 2), new KitItem(16, 1) }},
+                new Kit() { Cooldown = 10, Name = "Brute Force", Items = new List<KitItem>() { new KitItem(112, 1), new KitItem(113, 3), new KitItem(254, 3) }},
+                new Kit() { Cooldown = 10, Name = "Watcher", Items = new List<KitItem>() { new KitItem(109, 1), new KitItem(111, 3), new KitItem(236, 1) }}
+            };
+        }
     }
 
     public class Kit
