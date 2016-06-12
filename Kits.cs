5e9ed8240f8802588d9c93f8e35bc3f9b21f09b0
fr34kyn01535 <fr34kyn01535@bam.yt>
2016-06-12 13:35:12 +0200

    Removing kit cooldown

--- Kits.cs
+++ Kits.cs
@@ -12,9 +12,6 @@ namespace fr34kyn01535.Kits
     {
         public static Kits Instance = null;
 
-        public static Dictionary<string, DateTime> GlobalCooldown = new Dictionary<string,DateTime>();
-        public static Dictionary<string, DateTime> InvididualCooldown = new Dictionary<string, DateTime>();
-
         protected override void Load()
         {
             Instance = this;
@@ -36,8 +33,6 @@ namespace fr34kyn01535.Kits
                     {"command_kit_invalid_parameter","Invalid parameter, specify a kit with /kit <name>"},
                     {"command_kit_not_found","Kit not found"},
                     {"command_kit_no_permissions","You don't have permissions to use this kit"},
-                    {"command_kit_cooldown_command","You have to wait {0} seconds to use this command again"},
-                    {"command_kit_cooldown_kit","You have to wait {0} seconds to get this kit again"},
                     {"command_kit_failed_giving_item","Failed giving a item to {0} ({1},{2})"},
                     {"command_kit_success","You just received the kit {0}" },
                     {"command_kits","You have access to the following kits: {0}" },
--- KitsConfiguration.cs
+++ KitsConfiguration.cs
@@ -13,11 +13,10 @@ namespace fr34kyn01535.Kits
 
         public void LoadDefaults()
         {
-            GlobalCooldown = 10;
             Kits = new List<Kit>() {
-                new Kit() { Cooldown = 10, Name = "Survival", XP = 0,Items = new List<KitItem>() { new KitItem(245, 1), new KitItem(81, 2), new KitItem(16, 1) }},
-                new Kit() { Cooldown = 10, Name = "Brute Force", XP = 0,Money = 30, Vehicle = 57,Items = new List<KitItem>() { new KitItem(112, 1), new KitItem(113, 3), new KitItem(254, 3) }},
-                new Kit() { Cooldown = 10, Name = "Watcher", XP = 200,Money=-20, Items = new List<KitItem>() { new KitItem(109, 1), new KitItem(111, 3), new KitItem(236, 1) }}
+                new Kit() { Name = "Survival", XP = 0,Items = new List<KitItem>() { new KitItem(245, 1), new KitItem(81, 2), new KitItem(16, 1) }},
+                new Kit() { Name = "Brute Force", XP = 0,Money = 30, Vehicle = 57,Items = new List<KitItem>() { new KitItem(112, 1), new KitItem(113, 3), new KitItem(254, 3) }},
+                new Kit() { Name = "Watcher", XP = 200,Money=-20, Items = new List<KitItem>() { new KitItem(109, 1), new KitItem(111, 3), new KitItem(236, 1) }}
             };
         }
     }
