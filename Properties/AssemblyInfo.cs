35654554627d93fbd36cd4fc60114c137c300089
fr34kyn01535 <fr34kyn01535@bam.yt>
2016-06-16 23:01:03 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Votifier")]
-[assembly: AssemblyCompany("unturned.ROCKS")]
-[assembly: AssemblyProduct("Votifier")]
+[assembly: AssemblyTitle("Kits")]
+[assembly: AssemblyCompany("fr34kyn01535")]
+[assembly: AssemblyProduct("Kits")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: ComVisible(false)]
 [assembly: Guid("69c74bf2-70ab-42eb-baa5-0cbc09fc7e56")]
